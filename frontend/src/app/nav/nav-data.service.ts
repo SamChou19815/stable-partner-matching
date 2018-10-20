8de7577d4db783b398447e3cae52d47cafc3d465
Sam <sam@developersam.com>
2018-10-19 23:31:53 -0400

    Update frontend/src/app/nav/nav-data.service.ts

--- frontend/src/app/nav/nav-data.service.ts
+++ frontend/src/app/nav/nav-data.service.ts
@@ -9,71 +9,29 @@ export class NavDataService {
 
   /**
    * The group for scheduler.
-   * @type {NavGroup}
    */
-  private readonly schedulerNavGroup: NavGroup = <NavGroup>{
-    name: 'Scheduler', icon: Icon.ofMaterial('event_note'),
+  private readonly appGroup: NavGroup = <NavGroup>{
+    name: 'App', icon: Icon.ofMaterial('event_note'),
     children: [
       {
-        name: 'Projects', icon: Icon.ofMaterial('event_available'),
+        name: 'Profile', icon: Icon.ofMaterial('profile'),
         link: '/scheduler/projects'
       },
       {
-        name: 'Events', icon: Icon.ofMaterial('event'), link: '/scheduler/events'
+        name: 'Courses', icon: Icon.ofMaterial('school'), link: '/scheduler/events'
       },
       {
-        name: 'Friends', icon: Icon.ofMaterial('group'), link: '/friends'
+        name: 'Partners', icon: Icon.ofMaterial('group'), link: '/friends'
       },
       {
-        name: 'Auto Scheduling', icon: Icon.ofMaterial('dashboard'), link: '/scheduler/auto'
-      }
-    ]
-  };
-  /**
-   * Group for RSS Reader.
-   * @type {NavGroup}
-   */
-  private readonly rssReaderGroup: NavGroup = <NavGroup>{
-    name: 'RSS Reader', icon: Icon.ofMaterial('chrome_reader_mode'),
-    children: [
-      {
-        name: 'Articles', icon: Icon.ofMaterial('library_books'),
-        link: '/rss-reader/articles'
-      },
-      {
-        name: 'Subscriptions', icon: Icon.ofMaterial('rss_feed'),
-        link: '/rss-reader/subscriptions'
-      },
-    ]
-  };
-  /**
-   * The group for playground
-   * @type {NavGroup}
-   */
-  private readonly playgroundGroup: NavGroup = <NavGroup>{
-    name: 'Playground', icon: Icon.ofMaterial('apps'),
-    children: [
-      <NavItem>{
-        name: 'SAMPL', icon: Icon.ofMaterial('memory'),
-        link: '/playground/sampl'
-      },
-      <NavItem>{
-        name: 'Chunk Reader', icon: Icon.ofMaterial('speaker_notes'),
-        link: '/playground/chunk-reader'
-      },
-      <NavItem>{
-        name: 'TEN', icon: Icon.ofMaterial('grid_on'),
-        link: '/playground/ten'
+        name: 'Matching', icon: Icon.ofMaterial('dashboard'), link: '/scheduler/auto'
       }
     ]
   };
   /**
    * The nav data list for display.
-   * @type {NavDataList}
    */
-  readonly navDataList: NavDataList = new NavDataList(<NavGroup[]>[
-    this.schedulerNavGroup, this.rssReaderGroup, this.playgroundGroup
-  ]);
+  readonly navDataList: NavDataList = new NavDataList(<NavGroup[]>[this.appGroup]);
 
   constructor() {
   }
