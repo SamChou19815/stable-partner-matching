df3c5f954687010b606a8b4d905e30e84873725b
Sam <sam@developersam.com>
2018-10-20 14:04:13 -0400

    Refactor Max's code

--- backend/src/main/java/IntervalMergingUtil.java
+++ backend/src/main/java/IntervalMergingUtil.java
@@ -1,23 +1,30 @@
-import java.util.*;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+/**
+ * The utility to merge intervals.
+ */
 public final class IntervalMergingUtil {
-
+    
     /**
      * A method that merges intervals to ensure they're not overlapping in the end.
+     *
      * @param intervals: Initial list of intervals
      * @return intervals that doesn't overlap
      */
-    public List<Interval> mergeInterval (List<Interval> intervals) {
+    @NotNull
+    public static List<Interval> mergeInterval(@NotNull List<Interval> intervals) {
         // then compare the first
-        if (intervals.size() <=1) return intervals;
-        Collections.sort(intervals, (Comparator.comparing((Interval o) -> o.start).thenComparing(o -> o.end)));
-
-
-        List<Interval> mergedInterval = new ArrayList<>() {};
+        if (intervals.size() <= 1) {
+            return intervals;
+        }
+        intervals.sort((Comparator.comparing((Interval o) -> o.start).thenComparing(o -> o.end)));
+        List<Interval> mergedInterval = new ArrayList<Interval>() {};
         Interval last = intervals.get(0);
-        for (int i = 0; i < intervals.size(); i++){
-            Interval current = intervals.get(i);
+        for (Interval current : intervals) {
             if (last.end >= current.start) {
                 last = new Interval(last.start, Math.max(current.end, last.end));
             } else {
@@ -28,5 +35,5 @@ public final class IntervalMergingUtil {
         mergedInterval.add(last);
         return mergedInterval;
     }
-
+    
 }
