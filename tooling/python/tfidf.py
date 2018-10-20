c1a839bda9fd27582b2eb03e7294db6566e17c49
Ziwei Gu <zg48@cornell.edu>
2018-10-20 13:22:38 -0400

    Finished TF-IDF weighting script

--- tooling/python/tfidf.py
+++ tooling/python/tfidf.py
@@ -1,55 +1,64 @@
-import numpy
 import pandas as pd
-import nltk
 import re
-from nltk.stem.snowball import SnowballStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import json
+import sys
 
-courses = []
-keywords = []
+file = sys.argv[1]
+frame = pd.read_json(file)
+text = []
+for chunks in frame['keywords']:
+    acc = ''
+    for word in chunks:
+        acc += (word + ' ')
+    text.append(acc[:-1])
 
-ranks = []
-for i in range(len(courses)):
-    ranks.append(i)
-stopwords = nltk.corpus.stopwords.words('english')
-stopwords += ['course']
+def pre_process(text):
 
-stemmer = SnowballStemmer("english")
+    text=text.lower()
 
-def tokenize_and_stem(text):
-    tokens = [word for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
-    filtered_tokens = []
-    for token in tokens:
-        if re.search('[a-zA-Z]', token):
-            filtered_tokens.append(token)
-    stems = [stemmer.stem(t) for t in filtered_tokens]
-    return stems
+    text=re.sub("&lt;/?.*?&gt;"," &lt;&gt; ",text)
 
+    text=re.sub("(\\d|\\W)+"," ",text)
 
-def tokenize_only(text):
-    tokens = [word.lower() for sent in nltk.sent_tokenize(text) for word in nltk.word_tokenize(sent)]
-    filtered_tokens = []
-    for token in tokens:
-        if re.search('[a-zA-Z]', token):
-            filtered_tokens.append(token)
-    return filtered_tokens
+    return text
+text = [pre_process(t) for t in text]
 
-totalvocab_stemmed = []
-totalvocab_tokenized = []
-for i in keywords:
-    allwords_stemmed = tokenize_and_stem(i)
-    totalvocab_stemmed.extend(allwords_stemmed)
+cv=CountVectorizer(max_df=0.85, stop_words='english', max_features=10000)
+word_count_vector=cv.fit_transform(text)
+tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
+tfidf_transformer.fit(word_count_vector)
 
-    allwords_tokenized = tokenize_only(i)
-    totalvocab_tokenized.extend(allwords_tokenized)
+feature_names=cv.get_feature_names()
+doc = ''
+for descriptions in text:
+    doc += descriptions
+tf_idf_vector=tfidf_transformer.transform(cv.transform([doc]))
 
-vocab_frame = pd.DataFrame({'words': totalvocab_tokenized}, index = totalvocab_stemmed)
+def sort_coo(coo_matrix):
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
 
-tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=2000,
-                                 min_df=0.1, stop_words= stopwords,
-                                 use_idf=True, tokenizer=tokenize_and_stem, ngram_range=(1,3))
+def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+    sorted_items = sorted_items[:topn]
 
-tfidf_matrix = tfidf_vectorizer.fit_transform(keywords)
-print(tfidf_matrix.shape)
-terms = tfidf_vectorizer.get_feature_names()
-print(terms)
+    score_vals = []
+    feature_vals = []
+
+    for idx, score in sorted_items:
+        score_vals.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+    results= {}
+    for idx in range(len(feature_vals)):
+        results[feature_vals[idx]]=score_vals[idx]
+
+    return results
+
+sorted_items=sort_coo(tf_idf_vector.tocoo())
+keywords=extract_topn_from_vector(feature_names,sorted_items,round(0.5*len(text)))
+
+result = [k for k in keywords]
+
+with open('keywords.json', 'w') as outfile:
+    json.dump(result, outfile)
