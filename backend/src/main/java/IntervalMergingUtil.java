85f39c30b785d5adc2a3ceb74c72f5dd2a9a2e9e
Max Jiang <qj46@cornell.edu>
2018-10-20 14:01:07 -0400

    implement interval merging

--- backend/src/main/java/IntervalMergingUtil.java
+++ backend/src/main/java/IntervalMergingUtil.java
@@ -1,2 +1,32 @@
+import java.util.*;
+
+
 public final class IntervalMergingUtil {
+
+    /**
+     * A method that merges intervals to ensure they're not overlapping in the end.
+     * @param intervals: Initial list of intervals
+     * @return intervals that doesn't overlap
+     */
+    public List<Interval> mergeInterval (List<Interval> intervals) {
+        // then compare the first
+        if (intervals.size() <=1) return intervals;
+        Collections.sort(intervals, (Comparator.comparing((Interval o) -> o.start).thenComparing(o -> o.end)));
+
+
+        List<Interval> mergedInterval = new ArrayList<>() {};
+        Interval last = intervals.get(0);
+        for (int i = 0; i < intervals.size(); i++){
+            Interval current = intervals.get(i);
+            if (last.end >= current.start) {
+                last = new Interval(last.start, Math.max(current.end, last.end));
+            } else {
+                mergedInterval.add(last);
+                last = current;
+            }
+        }
+        mergedInterval.add(last);
+        return mergedInterval;
+    }
+
 }
