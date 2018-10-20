c109ba5ab77b8d66b07f7043ef192e259673b4ff
cc982 <cc982@cornell.edu>
2018-10-20 15:49:12 -0400

    Fixed compile issues

--- backend/src/main/java/Ranking.java
+++ backend/src/main/java/Ranking.java
@@ -1,5 +1,7 @@
 import auth.GoogleUser;
 import com.google.cloud.datastore.Key;
+import com.google.gson.Gson;
+import course.CourseInfo;
 import course.StudentCourse;
 import init.InitData;
 import partner.StudentPartnership;
@@ -8,26 +10,42 @@ import student.StudentPublicInfo;
 import java.util.List;
 import java.util.Vector;
 
+
 public class Ranking {
 
+    public Vector<Double> courseW;
+    public Vector<Double> emptyVector;
+
+    public void init(StudentCourse course) {
+        CourseInfo info = CourseInfo.Companion.get(course.getKey());
+        Gson gson = new Gson();
+        gson.fromJson(info.getWeightVector(), Vector.class);
+    }
     public int SkillSetSum(String skills) {
         return 100; // TODO
     }
 
     public Vector compute_s_score(Key userKey, Key courseKey) {
-        StudentPublicInfo info = StudentPublicInfo.Companion.buildForGeneral(userKey);
+        StudentPublicInfo studInfo = StudentPublicInfo.Companion.buildForGeneral(userKey);
 
-        List<Key> pastCourses = info.getPastCourses();
-        List<Key> currCourses = info.getCurrCourses();
-        String skills = info.getSkills();
-        
+        String skillStr = studInfo.getSkills();
+        String[] skills = skillStr.split(", ");
 
+
+        List<Key> pastCourses = studInfo.getPastCourses();
+        for (Key k : pastCourses) {
+            CourseInfo courseInfo = CourseInfo.Companion.get(k);
+        }
+
+//        List<Key> currCourses = info.getCurrCourses();
+
+        return null;
     }
     public void getRankingForCourse(GoogleUser user, StudentCourse course) {
         InitData data = InitData.Companion.getByUser(user);
         Key userKey =  user.getKey();
         Key courseKey = course.getKey();
-        user.getSkills()
+        user.getSkills();
 
 
     }
